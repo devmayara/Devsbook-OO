c2056f0a3c3a7182e84c78591588ac9e8bda5d53
Mayara Silva <devmayara11@gmail.com>
2022-08-26 14:18:55 -0300

    Estruturando Template

--- index.php
+++ index.php
@@ -5,5 +5,13 @@ require 'models/Auth.php';
 
 $auth = new Auth($pdo, $base);
 $userInfo = $auth->checkToken();
+$activeMenu = 'home';
 
 require 'partials/header.php';
+require 'partials/menu.php';
+?>
+
+
+<?php
+require 'partials/footer.php';
+?>
--- partials/header.php
+++ partials/header.php
@@ -4,34 +4,35 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="assets/css/style.css" />
+    <link rel="stylesheet" href="<?= $base; ?>assets/css/style.css" />
     <title></title>
 </head>
 <body>
     <header>
         <div class="container">
             <div class="logo">
-                <a href=""><img src="assets/images/devsbook_logo.png" /></a>
+                <a href=""><img src="<?= $base; ?>assets/images/devsbook_logo.png" /></a>
             </div>
             <div class="head-side">
                 <div class="head-side-left">
                     <div class="search-area">
-                        <form method="GET">
+                        <form action="<?= $base; ?>search.php" method="GET">
                             <input type="search" placeholder="Pesquisar" name="s" />
                         </form>
                     </div>
                 </div>
                 <div class="head-side-right">
-                    <a href="" class="user-area">
-                        <div class="user-area-text">Bonieky</div>
+                    <a href="<?= $base; ?>perfil.php" class="user-area">
+                        <div class="user-area-text"><?= $userInfo->name; ?></div>
                         <div class="user-area-icon">
-                            <img src="media/avatars/avatar.jpg" />
+                            <img src="<?= $base; ?>media/avatars/<?= $userInfo->avatar; ?>" />
                         </div>
                     </a>
-                    <a href="" class="user-logout">
-                        <img src="assets/images/power_white.png" />
+                    <a href="<?= $base; ?>logout.php" class="user-logout">
+                        <img src="<?= $base; ?>assets/images/power_white.png" />
                     </a>
                 </div>
             </div>
         </div>
     </header>
+    <section class="container main">
