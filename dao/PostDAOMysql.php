3143658fe2ffd82c41bf872960fb71be0a0166c9
Mayara Silva <devmayara11@gmail.com>
2022-08-27 07:39:07 -0300

    Página Home

--- dao/PostDAOMysql.php
+++ dao/PostDAOMysql.php
@@ -1,8 +1,10 @@
 <?php
 
 require_once 'models/Post.php';
+require_once 'dao/UserRelationDaoMysql.php';
+require_once 'dao/UserDaoMysql.php';
 
-class PostDAOMysql implements PostDAO 
+class PostDaoMysql implements PostDAO 
 {
     private $pdo;
 
@@ -24,5 +26,55 @@ class PostDAOMysql implements PostDAO
         $sql->bindValue(':body', $p->body);
         $sql->bindValue(':created_at', $p->created_at);
         $sql->execute();
+    }
+
+    public function getHomeFeed($id_user)
+    {
+        $array = [];
+
+        $urDao = new UserRelationDaoMysql($this->pdo);
+        $userList = $urDao->getRelationsFrom($id_user);
+        $userList[] = $id_user;
+
+        $sql = $this->pdo->query("SELECT * FROM posts
+        WHERE id_user IN (".implode(',', $userList).")
+        ORDER BY created_at DESC");
+        if($sql->rowCount() > 0) {
+            $data = $sql->fetchAll(PDO::FETCH_ASSOC);
+
+            $array = $this->_postListToObject($data, $id_user);
+        }
+
+        return $array;
+    }
+
+    private function _postListToObject($post_list, $id_user) 
+    {
+        $posts = [];
+        $userDao = new UserDaoMysql($this->pdo);
+
+        foreach($post_list as $post_item) {
+            $newPost = new Post();
+            $newPost->id = $post_item['id'];
+            $newPost->type = $post_item['type'];
+            $newPost->body = $post_item['body'];
+            $newPost->created_at = $post_item['created_at'];
+            $newPost->mine = false;
+
+            if($post_item['id_user'] == $id_user) {
+                $newPost->mine = true;
+            }
+
+            $newPost->user = $userDao->findById($post_item['id_user']);
+
+            $newPost->likeCount = 0;
+            $newPost->liked = false;
+
+            $newPost->comments = [];
+            
+            $posts[] = $newPost;
+        }
+        
+        return $posts;
     }
 }
--- index.php
+++ index.php
@@ -2,80 +2,29 @@
 
 require 'config.php';
 require 'models/Auth.php';
+require 'dao/PostDAOMysql.php';
 
 $auth = new Auth($pdo, $base);
 $userInfo = $auth->checkToken();
 $activeMenu = 'home';
+
+$postDao = new PostDAOMysql($pdo);
+$feed = $postDao->getHomeFeed($userInfo->id);
 
 require 'partials/header.php';
 require 'partials/menu.php';
 ?>
 
 <section class="feed mt-10">
-    
+
     <div class="row">
         <div class="column pr-5">
-            
+
             <?php require 'partials/feed-editor.php'; ?>
 
-            <!-- <div class="box feed-item">
-                <div class="box-body">
-                    <div class="feed-item-head row mt-20 m-width-20">
-                        <div class="feed-item-head-photo">
-                            <a href=""><img src="media/avatars/avatar.jpg" /></a>
-                        </div>
-                        <div class="feed-item-head-info">
-                            <a href=""><span class="fidi-name">Bonieky Lacerda</span></a>
-                            <span class="fidi-action">fez um post</span>
-                            <br/>
-                            <span class="fidi-date">07/03/2020</span>
-                        </div>
-                        <div class="feed-item-head-btn">
-                            <img src="assets/images/more.png" />
-                        </div>
-                    </div>
-                    <div class="feed-item-body mt-10 m-width-20">
-                        Pessoal, tudo bem! Busco parceiros para empreender comigo em meu software.<br/><br/>
-                        Acabei de aprová-lo na Appstore. É um sistema de atendimento via WhatsApp multi-atendentes para auxiliar empresas.<br/><br/>
-                        Este sistema permite que vários funcionários/colaboradores da empresa atendam um mesmo número de WhatsApp, mesmo que estejam trabalhando remotamente, sendo que cada um acessa com um login e senha particular....
-                    </div>
-                    <div class="feed-item-buttons row mt-20 m-width-20">
-                        <div class="like-btn on">56</div>
-                        <div class="msg-btn">3</div>
-                    </div>
-                    <div class="feed-item-comments">
-                        
-                        <div class="fic-item row m-height-10 m-width-20">
-                            <div class="fic-item-photo">
-                                <a href=""><img src="media/avatars/avatar.jpg" /></a>
-                            </div>
-                            <div class="fic-item-info">
-                                <a href="">Bonieky Lacerda</a>
-                                Comentando no meu próprio post
-                            </div>
-                        </div>
-
-                        <div class="fic-item row m-height-10 m-width-20">
-                            <div class="fic-item-photo">
-                                <a href=""><img src="media/avatars/avatar.jpg" /></a>
-                            </div>
-                            <div class="fic-item-info">
-                                <a href="">Bonieky Lacerda</a>
-                                Muito legal, parabéns!
-                            </div>
-                        </div>
-
-                        <div class="fic-answer row m-height-10 m-width-20">
-                            <div class="fic-item-photo">
-                                <a href=""><img src="media/avatars/avatar.jpg" /></a>
-                            </div>
-                            <input type="text" class="fic-item-field" placeholder="Escreva um comentário" />
-                        </div>
-
-                    </div>
-                </div>
-            </div> -->
-
+            <?php foreach($feed as $Item): ?>
+                <?php require 'partials/feed-item.php'; ?>
+            <?php endforeach; ?>
 
         </div>
         <div class="column side pl-5">
@@ -83,7 +32,7 @@ require 'partials/menu.php';
                 <div class="box-header">
                     <div class="box-header-text">Patrocinios</div>
                     <div class="box-header-buttons">
-                        
+
                     </div>
                 </div>
                 <div class="box-body">
